e8b3f0ae346cb3e9db51d8d5cb35c4a0f370acf1
Main User <thecallummckenna@proton.me>
2025-11-15 14:34:27 -0700

    errors

--- flake.nix
+++ flake.nix
@@ -2,16 +2,16 @@
   description = "A general purpose development shell";
 
   inputs = {
-    nixpkgs.url = "github:nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = nixpkgs.legacyPackages."${system}";
   in
   {
-    devshells.${system}.default = pkgs.myShell {
+    devshells."${system}".default = pkgs.myShell {
       packages = with pkgs; [
         gcc
       ];
